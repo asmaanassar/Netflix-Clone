65a30a856ad3417790f364747ec3eec3adf74c7e
asmaanassar <asmaanassar89@gmail.com>
2022-02-16 15:56:37 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './component/Home/Home';
-function App() {
-  return (
-    <div className="App">
-     <Routes>
-          <Route path='/' exact  element={<Home/>} />
-      </Routes>
-    </div>
-  );
-}
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
+  function App() {
+    return (
+      <div className="App">
+        <Header />
+        <Routes>
+           <Route path='/' element={<Home/>} />
+        </Routes>
+        <Footer />
+      </div>
+    );
+  }
 
 export default App;
---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,21 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="top-footer">
+                <h3>Enjoy To Watching Movie</h3>
+            
+            <nav>
+                    <a href="https://web.facebook.com/">facebook</a>
+                    <a href="https://www.instagram.com/">instagram</a>
+                </nav>
+                <div className="footer-copyright">
+                    &copy; 2022 By Asma
+                </div>
+                </div>
+        </div>
+    )
+}
+
+export default Footer; 
